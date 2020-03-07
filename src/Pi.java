b5009af370585088f3513c611bf483d7df5884a6
ripboss <cmr15765@campus.fct.unl.pt>
2020-03-07 23:20:14 +0000

    minor changes

--- src/Pi.java
+++ src/Pi.java
@@ -5,7 +5,7 @@ public class Pi {
 		// TODO Auto-generated method stub
 		int n = Integer.parseInt(args[0]);
 		int within = 0;
-		long ti = System.nanoTime();
+//		long ti = System.nanoTime();
 		for (int i=0; i<n; i++) {
 			double x = Math.random();
 			double y = Math.random();
@@ -13,10 +13,10 @@ public class Pi {
 			if (test <= 1)
 				within++;	
 		}
-		long tf = System.nanoTime();
+//		long tf = System.nanoTime();
 		double pi = ((double)within/n) * 4;
 		
-		System.out.println((tf-ti)/1000000 + "ms");
+//		System.out.println((tf-ti)/1000000 + "ms");
 		System.out.println("Total Number of points: " + n);
 		System.out.println("Points within circle: " + within);
 		System.out.println("Pi estimation: " + pi);
